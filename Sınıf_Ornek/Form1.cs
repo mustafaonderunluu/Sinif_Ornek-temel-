a316c431108d66885f3d82065a3787be55f6c1cc
Mustafa Önder ÜNLÜ <116373504+mustafaonderunluu@users.noreply.github.com>
2024-02-01 14:14:56 +0300

    Proje dosyası ekle.

---
+++ Sınıf_Ornek/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sınıf_Ornek
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Araba rb=new Araba();
+            rb.renk = "Kırmızı";
+            rb.hız = 200;
+            rb.motor = 1245.56;
+            rb.fiyat = 50000;
+            rb.durum = 's';
+
+            label1.Text= rb.renk;
+            label2.Text=rb.hız.ToString();
+            label4.Text=rb.motor.ToString();
+            label5.Text=rb.fiyat.ToString();
+            label6.Text=rb.durum.ToString();
+          
+        }
+    }
+}
---
+++ Sınıf_Ornek/Form2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sınıf_Ornek
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            Araba rb2= new Araba();
+            rb2.renk = "Yeşil";
+            rb2.hız = 140;
+            rb2.motor = 1236.15;
+            rb2.durum = '2';
+            rb2.fiyat = 35658;
+
+            label1.Text= rb2.renk;
+            label2.Text= rb2.hız.ToString();
+            label3.Text= rb2.motor.ToString();
+            label4.Text= rb2.durum.ToString();
+            label5.Text=rb2.fiyat.ToString();
+
+
+            
+        }
+    }
+}
